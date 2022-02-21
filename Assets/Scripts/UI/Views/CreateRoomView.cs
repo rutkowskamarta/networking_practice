464dfbea71762e8f78db34e9f6cdf4e690a1a007
Lady Marta <lady.marta@theknightsofunity.com>
2022-02-21 18:32:34 +0100

    Create room wip.

--- Assets/Scripts/UI/Views/CreateRoomView.cs
+++ Assets/Scripts/UI/Views/CreateRoomView.cs
@@ -1,7 +1,44 @@
+using Game.Client;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
 namespace Game.UI
 {
     public class CreateRoomView : UIView
     {
+        [SerializeField]
+        private Button createRoom;
+        [SerializeField]
+        private Button backToMenu;
 
-    }
+		[Inject]
+		private IGameClientManager gameClientManager;
+
+		public override void Show(Action onShownCallback = null)
+		{
+			base.Show(onShownCallback);
+			createRoom.onClick.AddListener(CreateRoomButton_OnClick);
+			backToMenu.onClick.AddListener(BackToMenuButton_OnClick);
+		}
+
+		public override void Hide(Action onHiddenCallback = null)
+		{
+			base.Hide(onHiddenCallback);
+			createRoom.onClick.RemoveListener(CreateRoomButton_OnClick);
+			backToMenu.onClick.RemoveListener(BackToMenuButton_OnClick);
+		}
+
+		private void CreateRoomButton_OnClick()
+		{
+
+		}
+
+		private void BackToMenuButton_OnClick()
+		{
+			uiViewsManager.ShowViewOfType(UIViewType.MainMenu);
+		}
+
+	}
 }
